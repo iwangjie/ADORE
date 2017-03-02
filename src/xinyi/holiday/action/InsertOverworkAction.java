9bf33cc1affcd131a0cc16800085ad5e18050438
ShawSong <songshawyx@163.com>
2017-03-02 09:40:35 +0800

    :hammer::refactor:InsertOverworkAction.java:新意加班action功能修改。

--- src/xinyi/holiday/action/InsertOverworkAction.java
+++ src/xinyi/holiday/action/InsertOverworkAction.java
@@ -5,10 +5,6 @@ import weaver.general.BaseBean;
 import weaver.general.Util;
 import weaver.interfaces.workflow.action.Action;
 import weaver.soa.workflow.request.RequestInfo;
-import xinyi.holiday.util.InsertUtil;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by adore on 2017/1/19.
@@ -23,23 +19,8 @@ public class InsertOverworkAction implements Action {
 
         RecordSet rs = new RecordSet();
 
-        InsertUtil IU = new InsertUtil();
-
         String sql = "";
         String tableName = "";
-        String workflowCode = "";//流程编号
-        String applicant = "";//申请人
-        String applyDate = "";//申请日期
-        String applyDept = "";//申请部门
-        String overworkType = "";//加班类型
-        String overStartDate = "";//申请加班日期
-        String overStartTime = "";//申请加班时间
-        String overEndDate = "";//加班结束日期
-        String overEndTime = "";//加班结束时间
-        String overTime = "";//加班小时数
-        String reason = "";//加班事由
-        //String status = "";//状态
-        //String belongto = "";//归属月份
         String requestid = info.getRequestid();
         String workflowid = info.getWorkflowid();
 
@@ -51,42 +32,9 @@ public class InsertOverworkAction implements Action {
         }
 
         if (!"".equals(tableName)) {
-
-            String sql_1 = "select * from " + tableName + " where requestid = " + requestid;
-            //log.writeLog("sql1---------" + sql_1);
-            rs.executeSql(sql_1);
-            if (rs.next()) {
-                workflowCode = Util.null2String(rs.getString("lcbh"));
-                applicant = Util.null2String(rs.getString("sqr"));
-                applyDate = Util.null2String(rs.getString("sqrq"));
-                applyDept = Util.null2String(rs.getString("sqbm"));
-                overworkType = Util.null2String(rs.getString("jblx"));
-                overStartDate = Util.null2String(rs.getString("sqjbrq"));
-                overStartTime = Util.null2String(rs.getString("sqjbsj"));
-                overEndDate = Util.null2String(rs.getString("jbjsrq"));
-                overEndTime = Util.null2String(rs.getString("jbjssj"));
-                overTime = Util.null2String(rs.getString("jbxss"));
-                reason = Util.null2String(rs.getString("jbsy"));
-
-                Map<String, String> mapStr_M = new HashMap<String, String>();
-                mapStr_M.put("lcbh", workflowCode);
-                mapStr_M.put("sqr", applicant);
-                mapStr_M.put("sqrq", applyDate);
-                mapStr_M.put("sqbm", applyDept);
-                mapStr_M.put("jblx", overworkType);
-                mapStr_M.put("sqjbrq", overStartDate);
-                mapStr_M.put("sqjbsj", overStartTime);
-                mapStr_M.put("jbjsrq", overEndDate);
-                mapStr_M.put("jbjssj", overEndTime);
-                mapStr_M.put("jbxss", overTime);
-                mapStr_M.put("jbsy", reason);
-                mapStr_M.put("belongto", "##CONVERT(varchar(7) ,GETDATE(), 23 )");
-                //mapStr_M.put("modedatacreatetime", "##CONVERT(varchar(5) ,GETDATE(), 114 )");
-                mapStr_M.put("status", "0");
-
-                String table_M = "formtable_main_141";
-                IU.insert(mapStr_M, table_M);
-            }
+            String proc_insert = " exec xy_overWorkAdd_proc " + requestid + " ";
+            log.writeLog("proc_insert=" + proc_insert);
+            rs.executeSql(proc_insert);
         } else {
             log.writeLog("流程信息表读取错误");
             return "-1";
