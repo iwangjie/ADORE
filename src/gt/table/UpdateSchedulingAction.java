a96d2f0385a152f8a2c968721f554e795443c84d
ShawSong <songshawyx@163.com>
2017-04-27 15:42:17 +0800

    UpdateSchedulingAction.java

--- src/gt/table/UpdateSchedulingAction.java
+++ src/gt/table/UpdateSchedulingAction.java
@@ -8,52 +8,52 @@ import weaver.soa.workflow.request.RequestInfo;
 
 public class UpdateSchedulingAction implements Action {
 
-	public String execute(RequestInfo info) {
-		new BaseBean().writeLog("进入排班UpdateSchedulingAction");
+    public String execute(RequestInfo info) {
+        new BaseBean().writeLog("进入排班UpdateSchedulingAction");
 
-		RecordSet rs = new RecordSet();
-		RecordSet res = new RecordSet();
-		String sql = "";
-		String[] ArrangedStaff = new String[100];
-		String tmp_ArrangedStaff = "";
-		String WorkStartTime = "";
-		String WorkEndTime = "";
+        RecordSet rs = new RecordSet();
+        RecordSet res = new RecordSet();
+        String sql = "";
+        String[] ArrangedStaff = new String[100];
+        String tmp_ArrangedStaff = "";
+        String WorkStartTime = "";
+        String WorkEndTime = "";
 
-	    sql = " select * from formtable_main_124 ";
-		new BaseBean().writeLog("sql1---------" + sql);
-		rs.executeSql(sql);
-		while (rs.next()) {
-			WorkStartTime = Util.null2String(rs.getString("startTime"));
-			WorkEndTime = Util.null2String(rs.getString("endTime"));
-			ArrangedStaff = Util.null2String(rs.getString("EmployeeName")).split(",");
-			for (int i = 0; i < ArrangedStaff.length; i++) {
-				tmp_ArrangedStaff = ArrangedStaff[i];
-				// new BaseBean().writeLog("tmp_ArrangedStaff = " +
-				// tmp_ArrangedStaff);
-				if (!"".equals(tmp_ArrangedStaff)) {
-					sql = " select count(*) as emp_num from emp_work_normal_time where emp_id = "+tmp_ArrangedStaff;
-					res.executeSql(sql);
-					if(res.next()){
-						String emp_num = Util.null2String(res.getString("emp_num"));
-						if("0".equals(emp_num)){
-							sql = " insert into emp_work_normal_time values ("+tmp_ArrangedStaff+",'"+WorkStartTime+"','"+WorkEndTime+"') ";
-						}else{
-							sql = " update emp_work_normal_time set normal_start_time = '"+WorkStartTime+"',normal_end_time = '"+WorkEndTime+"'where emp_id = "+tmp_ArrangedStaff;
-						}
-					}
-					new BaseBean().writeLog("sql_up---------" + sql);
-					if (!rs.executeSql(sql)) {
-						new BaseBean().writeLog("排班信息更新失败");
-						return "-1";
-					}
-				} else {
-					new BaseBean().writeLog("人员ID获取错误");
-					return "-1";
-				}
-			}
-		}
+        sql = " select * from formtable_main_124 ";
+        new BaseBean().writeLog("sql1---------" + sql);
+        rs.executeSql(sql);
+        while (rs.next()) {
+            WorkStartTime = Util.null2String(rs.getString("startTime"));
+            WorkEndTime = Util.null2String(rs.getString("endTime"));
+            ArrangedStaff = Util.null2String(rs.getString("EmployeeName")).split(",");
+            for (int i = 0; i < ArrangedStaff.length; i++) {
+                tmp_ArrangedStaff = ArrangedStaff[i];
+                // new BaseBean().writeLog("tmp_ArrangedStaff = " +
+                // tmp_ArrangedStaff);
+                if (!"".equals(tmp_ArrangedStaff)) {
+                    sql = " select count(*) as emp_num from emp_work_normal_time where emp_id = " + tmp_ArrangedStaff;
+                    res.executeSql(sql);
+                    if (res.next()) {
+                        String emp_num = Util.null2String(res.getString("emp_num"));
+                        if ("0".equals(emp_num)) {
+                            sql = " insert into emp_work_normal_time values (" + tmp_ArrangedStaff + ",'" + WorkStartTime + "','" + WorkEndTime + "') ";
+                        } else {
+                            sql = " update emp_work_normal_time set normal_start_time = '" + WorkStartTime + "',normal_end_time = '" + WorkEndTime + "'where emp_id = " + tmp_ArrangedStaff;
+                        }
+                    }
+                    new BaseBean().writeLog("sql_up---------" + sql);
+                    if (!rs.executeSql(sql)) {
+                        new BaseBean().writeLog("排班信息更新失败");
+                        return "-1";
+                    }
+                } else {
+                    new BaseBean().writeLog("人员ID获取错误");
+                    return "-1";
+                }
+            }
+        }
 
-		return SUCCESS;
-	}
+        return SUCCESS;
+    }
 
 }
