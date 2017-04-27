82da7723ea4f9d8b747a802e05123ab62ab23929
ShawSong <songshawyx@163.com>
2017-04-27 15:44:07 +0800

    BeforeDate.java

--- src/shaw/BeforeDate.java
+++ src/shaw/BeforeDate.java
@@ -1,7 +1,9 @@
 package shaw;
 
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -27,5 +29,20 @@ public class BeforeDate {
         BeforeDate DAU= new BeforeDate();
         String newDate=DAU.beforeDay("2016-03-30");
         System.out.println("newDate="+newDate);
+
+        String date1 = "2006-12-10";
+
+        DateFormat df = new SimpleDateFormat("yyyy-MM");
+        try {
+            Date d1 = df.parse(date1);
+            System.out.println("d1=="+df.format(d1));
+            Calendar  g = Calendar.getInstance();
+            g.setTime(d1);
+            g.add(Calendar.MONTH,+1);
+            Date d2 = g.getTime();
+            System.out.println("d2======="+df.format(d2));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
